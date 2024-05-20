75217c12823e09db327838966db9715525d26e42
kennethn23 <145921389+DubNubz@users.noreply.github.com>
2024-05-20 09:24:19 -0400

    create login

--- main/pages/index.vue
+++ main/pages/index.vue
@@ -1,13 +1,13 @@
-<script setup lang="ts">
-const route = useRoute()
-</script>
-
 <template>
   <div>
-    <h1>Nuxt Routing set up successfully!</h1>
-    <p>Current route: {{ route.path }}</p>
-    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
+    <h1>a</h1>
   </div>
-
-  <Card/>
 </template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ main/pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="holder">
+        <LoginForm @login="sendToPage" />
+    </div>
+</template>
+
+<script setup lang="ts">
+
+function sendToPage() {
+    // route -> /
+}
+
+</script>
+
+<style scoped>
+
+</style>
